44c05fe585343441c37654a2693b02ebf888249e
siddharthmishra95 <siiid.mishra@gmail.com>
2020-07-09 02:00:25 +0530

    Routing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 ///////////////////////////
 ////        FILES       ///
@@ -36,6 +37,19 @@ const http = require('http');
 ///////         SERVER          ///////////
 
 const server = http.createServer((req, res) => {
+  //  console.log(req.url);
+  const pathName = req.url;
+  if(pathName === '/' || pathName === '/overview') {
+      res.end('This is the OVERVIEW');
+  }else if (pathName === 'product') {
+    res.end('This is the PRODUCT')
+  } else {
+      res.writeHead(404, {
+          'Content-type':'text/html',
+          'my-own-header':'hello-world'
+      });
+      res.end('<h1>Page not found!</h1>');
+  }
     res.end('Hello from the Server!');
 });
 
