d1d2ec5f3b05a70ea6af2a22c35b086de5c13092
siddharthmishra95 <siiid.mishra@gmail.com>
2020-07-09 01:28:03 +0530

    Asynchronus Node handling

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const fs = require('fs');
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-console.log(textIn);
+//Blocking, synchronus way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-const textOut = `This is what we know about Lorem Ipsum: ${textIn}. \n Created on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('Files Written');
+// console.log(textIn);
+
+// const textOut = `This is what we know about Lorem Ipsum: ${textIn}. \n Created on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('Files Written');
+
+//Non-Blocking Asynchronus Way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log('ERROR! 💣');
+
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your File has been Written🙋');
+            })
+        });
+    });
+});
+console.log('Will Read file!');
